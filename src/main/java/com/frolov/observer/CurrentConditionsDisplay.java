4ba2a65324c9d092985150772d92c8aade56088b
frolov <frolov@iqmen.ru>
2019-04-09 16:47:31 +0300

    implementations

---
+++ src/main/java/com/frolov/observer/CurrentConditionsDisplay.java
@@ -0,0 +1,13 @@
+package com.frolov.observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement{
+    @Override
+    public void display() {
+        
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+
+    }
+}
