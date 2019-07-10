5eeecc2172d5cd818ef7918a1831ccdd27ecbd15
frolov <frolov@iqmen.ru>
2019-07-10 08:56:17 +0300

    Factory

---
+++ src/main/java/com/frolov/factory/PepperoniPizza.java
@@ -0,0 +1,26 @@
+package com.frolov.factory;
+
+public class PepperoniPizza implements Pizza {
+    @Override
+    public void prepare() {
+        System.out.println("Preparing pepperoni pizza");
+    }
+
+    @Override
+    public void bake() {
+        System.out.println("Baking pepperoni pizza");
+
+    }
+
+    @Override
+    public void cut() {
+        System.out.println("Cutting pepperoni pizza");
+
+    }
+
+    @Override
+    public void box() {
+        System.out.println("Boxing pepperoni pizza");
+
+    }
+}
---
+++ src/main/java/com/frolov/factory/Pizza.java
@@ -0,0 +1,8 @@
+package com.frolov.factory;
+
+public interface Pizza {
+    void prepare();
+    void bake();
+    void cut();
+    void box();
+}
